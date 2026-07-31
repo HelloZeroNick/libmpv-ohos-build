30bb2a79ff7989fa44563d3b3a553cfa4e022039
cnoim <cnoim@qq.com>
2026-07-31 15:10:57 +0800

    Update download-sdk.sh

--- download/download-sdk.sh
+++ download/download-sdk.sh
@@ -11,9 +11,10 @@ sudo mkdir -p sdk
 sudo tar -C sdk -zxf sdk.tar.gz
 sudo rm sdk.tar.gz
 
-cd sdk
-mv ohos-sdk ./../
+cd sdk/ohos-sdk
 sudo rm -rf windows/
+sudo rm -rf ohos/
+sudo mv linux ./../
 
 # Extract NDK
 cd linux
